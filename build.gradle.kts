8087d96569ca3a24db032dd44bf8229003354ea3
Glom Zhao <glom@skillw.com>
2024-12-07 08:34:44 +0800

    upgrade to intellij.platform-2.1.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,28 +1,34 @@
+import com.github.jengelman.gradle.plugins.shadow.tasks.ShadowJar
 import org.jetbrains.kotlin.gradle.dsl.JvmTarget
 
 plugins {
     id("java")
     id("org.jetbrains.kotlin.jvm") version "2.0.20"
-    id("org.jetbrains.intellij") version "1.17.4"
+    id("org.jetbrains.intellij.platform") version "2.1.0"
+    id("com.gradleup.shadow") version "8.3.5"
     kotlin("plugin.serialization") version "2.0.20"
 }
 
 group = "dev.codetime"
-version = "1.0.2"
+version = "1.0.3"
 
 repositories {
     mavenCentral()
+    intellijPlatform {
+
+        defaultRepositories()
+    }
 }
 
 // Configure Gradle IntelliJ Plugin
 // Read more: https://plugins.jetbrains.com/docs/intellij/tools-gradle-intellij-plugin.html
-intellij {
-    version.set("2023.2.6")
-    type.set("IC") // Target IDE Platform
-
-    plugins.set(listOf(/* Plugin Dependencies */))
+intellijPlatform {
+    pluginConfiguration {
+        name = "Code Time"
+    }
 }
 
+val ktorVersion: String by project
 tasks {
     // Set the JVM compatibility versions
     withType<JavaCompile> {
@@ -34,10 +40,9 @@ tasks {
             jvmTarget.set(JvmTarget.JVM_17)
         }
     }
-
-    patchPluginXml {
-        sinceBuild.set("232")
-        untilBuild.set("242.*")
+    prepareSandbox {
+        dependsOn(shadowJar)
+        from(shadowJar)
     }
 
     signPlugin {
@@ -49,14 +54,23 @@ tasks {
     publishPlugin {
         token.set(System.getenv("PUBLISH_TOKEN"))
     }
+    shadowJar {
+        archiveClassifier.set("")
+    }
 }
 
-val ktorVersion: String by project
 
 dependencies {
+    intellijPlatform {
+        intellijIdeaUltimate("2024.3")
+        instrumentationTools()
+        jetbrainsRuntime()
+
+    }
     implementation("org.slf4j:slf4j-nop:2.0.7")
     implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.7.3")
     implementation("io.ktor:ktor-client-core:$ktorVersion")
     implementation("io.ktor:ktor-client-cio:$ktorVersion")
 
 }
+
