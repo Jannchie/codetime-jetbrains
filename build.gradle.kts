8fc103d86b278c8656fe5161364da9b112a41b35
Glomzzz <glom@skillw.com>
2024-12-08 17:25:10 +0800

    upgrade to intellij.platform-2.1.0 to support latest Intellij IDE (#1)
    * upgrade to intellij.platform-2.1.0
    
    * github actions
    
    * github actions: grant execute permission for gradlew
    
    * add pluginVerifier in gradle.kts dependencies
    
    * add pluginVerification.ides in gradle.kts
    
    * remove recommended() in pluginVerification.ides
    
    * add pluginVerification.ides in gradle.kts
    
    * upgrade upload-artifact to v4
    
    * upgrade actions
    
    * remove shadowJar
    
    * ./gradlew verifyPlugin
    
    * fix  Release Jar in github actions
    
    * upgrade to actions/download-artifact@v4
    
    * remove gradle/wrapper-validation-action@v1
    
    * correct artifacts' name
    
    * correct the suffix
    
    * full version name
    
    * move properties to gradle.properties
    
    * correct version name

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,26 +1,29 @@
+import org.jetbrains.intellij.platform.gradle.IntelliJPlatformType
+import org.jetbrains.intellij.platform.gradle.TestFrameworkType
 import org.jetbrains.kotlin.gradle.dsl.JvmTarget
 
 plugins {
     id("java")
     id("org.jetbrains.kotlin.jvm") version "2.0.20"
-    id("org.jetbrains.intellij") version "1.17.4"
+    id("org.jetbrains.intellij.platform") version "2.1.0"
     kotlin("plugin.serialization") version "2.0.20"
 }
 
-group = "dev.codetime"
-version = "1.0.2"
 
 repositories {
     mavenCentral()
+    intellijPlatform {
+
+        defaultRepositories()
+    }
 }
 
 // Configure Gradle IntelliJ Plugin
 // Read more: https://plugins.jetbrains.com/docs/intellij/tools-gradle-intellij-plugin.html
-intellij {
-    version.set("2023.2.6")
-    type.set("IC") // Target IDE Platform
-
-    plugins.set(listOf(/* Plugin Dependencies */))
+intellijPlatform {
+    pluginConfiguration {
+        name = "Code Time"
+    }
 }
 
 tasks {
@@ -35,11 +38,6 @@ tasks {
         }
     }
 
-    patchPluginXml {
-        sinceBuild.set("232")
-        untilBuild.set("242.*")
-    }
-
     signPlugin {
         certificateChain.set(System.getenv("CERTIFICATE_CHAIN"))
         privateKey.set(System.getenv("PRIVATE_KEY"))
@@ -50,13 +48,34 @@ tasks {
         token.set(System.getenv("PUBLISH_TOKEN"))
     }
 }
-
 val ktorVersion: String by project
 
+intellijPlatform {
+    // ...
+
+    pluginVerification {
+        // ...
+
+        ides {
+            ide(IntelliJPlatformType.IntellijIdeaUltimate, "2024.3")
+        }
+    }
+}
 dependencies {
+    intellijPlatform {
+        intellijIdeaUltimate("2024.3")
+        bundledPlugin("com.intellij.java")
+
+        pluginVerifier()
+        zipSigner()
+        instrumentationTools()
+
+        testFramework(TestFrameworkType.Platform)
+    }
     implementation("org.slf4j:slf4j-nop:2.0.7")
     implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.7.3")
     implementation("io.ktor:ktor-client-core:$ktorVersion")
     implementation("io.ktor:ktor-client-cio:$ktorVersion")
 
 }
+
